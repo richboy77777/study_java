0966a15b5b6f1ef8760bf0673a07961d437167d6
richboy77777 <goodw@DESKTOP-AFQRPVH>
2022-02-03 16:48:35 +0900

    2월 3일 목요일

--- src/com/urdomain/lecture/ch02/Hello.java
+++ src/com/urdomain/lecture/ch02/Hello.java
@@ -4,6 +4,17 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+
+		int a; //선언
+		a = 1; //대입
+		
+		int b = 2;
+		
+		int c = 0;
+		
+		c = a + b;
+		
+		System.out.println(c);
 		
 	}
 
